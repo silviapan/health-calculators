3dfc770255875c05a3f69926ee58770381b1f9c9
Silvia Pan <silvia.pan29@gmail.com>
2018-11-27 22:16:24 -0500

    toggle show content

--- JS/events.js
+++ JS/events.js
@@ -1,10 +1,13 @@
 function toggleShow(event) {
   const menuOptions = document.getElementsByClassName('menu-option');
+  const contentSections = document.getElementsByClassName('content');
   for  (let i = 0; i < menuOptions.length; i++) {
     if (i === parseInt(event.dataset.index)) {
       menuOptions[i].classList.add('option-selected');
+      contentSections[i].classList.add('content-displayed');
     } else {
       menuOptions[i].classList.remove('option-selected');
+      contentSections[i].classList.remove('content-displayed');
     }
   }
 }
